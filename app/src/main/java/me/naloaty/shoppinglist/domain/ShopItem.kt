604b1804bf632c3c0d78e1f7fbd0ddbfeb8d4385
Naloaty <53543446+Naloaty@users.noreply.github.com>
2022-08-24 18:43:04 +0500

    Add simple shop list repository implementation

--- app/src/main/java/me/naloaty/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/me/naloaty/shoppinglist/domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package me.naloaty.shoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
     val title: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
