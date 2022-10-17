50ee55700521f1b64e0effce344b2af2a3e053d0
Naloaty <53543446+Naloaty@users.noreply.github.com>
2022-10-17 13:04:44 +0500

    Introduce Data Binding & apply to Recycler View

--- app/src/main/java/me/naloaty/shoppinglist/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/me/naloaty/shoppinglist/presentation/ShopItemViewHolder.kt
@@ -1,11 +1,8 @@
 package me.naloaty.shoppinglist.presentation
 
-import android.view.View
-import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import me.naloaty.shoppinglist.R
 
-class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-    val tvTitle = view.findViewById<TextView>(R.id.tv_title)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-}
+class ShopItemViewHolder(
+    val binding: ViewDataBinding
+): RecyclerView.ViewHolder(binding.root)
